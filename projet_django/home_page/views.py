189d1dbbacf96e9c310688050279536c52a9bfad
Tsiaro Yohan <tsiaroniainayohan@gmail.com>
2026-01-25 16:04:26 +0300

    login

--- projet_django/home_page/views.py
+++ projet_django/home_page/views.py
@@ -1,8 +1,48 @@
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        first_name = request.POST["first_name"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            email=email,
+            password=password
+        )
+
+        return redirect("login")
+
+    return render(request, "register.html")
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        # username = request.POST["password"]
+        user = authenticate(request, email=email, password=password)
+        # print(email)
+        # print(password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        
+        else:
+            return HttpResponse(user)
+            # return HttpResponse("Mot de passe ou email invalide")
+         
+    return render(request, "login.html")   
+    # template = loader.get_template("login.html")
+    # return HttpResponse(template.render())
+
 def home(request):
     template = loader.get_template("home/index.html")
     
